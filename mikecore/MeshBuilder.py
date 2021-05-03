ae198f536a39aafe48ff0da94e1b2e3ac8643ebf
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-05-03 21:37:39 +0200

    CreateMesh

--- mikecore/MeshBuilder.py
+++ mikecore/MeshBuilder.py
@@ -1,5 +1,5 @@
 from mikecore.DfsuFile import DfsuFile
-
+from mikecore.eum import eumQuantity, eumItem, eumUnit
 class MeshBuilder:
     Projection = None
     EumQuantity = None
@@ -66,136 +66,123 @@ class MeshBuilder:
 
     # }
 
+    # <summary>
+    # Validate will return a string of issues from the item builder.
+    # When this returns an empty list, the item has been properly build.
+    # </summary>
+    public string[] Validate()
+    {
+      return (Validate(false));
+    }
+
+    private string[] Validate(bool dieOnError)
+    {
+      List<string> errors = new List<string>();
+
+      if (!_isSetProjection)
+        errors.Add("Projection has not been set");
+      if (!_isSetNodes)
+        errors.Add("Nodes have not been set");
+      if (!_isSetConnectivity)
+        errors.Add("Elements have not been set");
+
+      ## Check that all nodenumbers are within the range of
+      ## number of nodes.
+      if (_isSetNodes && _isSetConnectivity)
+      {
+        bool check = true;
+        foreach (int[] elmt in _connectivity)
+        {
+          foreach (int nodeNumber in elmt)
+          {
+            if (0 >= nodeNumber || nodeNumber > _x.Length)
+            {
+              check = false;
+              break;
+            }
+          }
+          if (!check)
+            break;
+        }
+        if (!check)
+          errors.Add("At least one element has an invalid node number. Node numbers must be within [1,numberOfNodes]");
+      }
+
+      if (dieOnError && errors.Count > 0)
+      {
+        string msgs = DfsBuilder.ErrorMessage(errors);
+        throw new DfsException(msgs);
+      }
+
+      return (errors.ToArray());
+    }
+
     ## <summary>
-    ## Validate will return a string of issues from the item builder.
-    ## When this returns an empty list, the item has been properly build.
+    ## Create and return a new <see cref="MeshFile"/> object
     ## </summary>
-    # public string[] Validate()
-    # {
-    #   return (Validate(false));
-    # }
+    def MeshFile CreateMesh(self):
+        #Validate(true);
 
-    # private string[] Validate(bool dieOnError)
-    # {
-    #   List<string> errors = new List<string>();
+        ## Creating default eumQuantity in meters
+        if self.EumQuantity == None: 
+            self.EumQuantity = eumQuantity(eumItem.eumIBathymetry, eumUnit.eumUmeter);
 
-    #   if (!_isSetProjection)
-    #     errors.Add("Projection has not been set");
-    #   if (!_isSetNodes)
-    #     errors.Add("Nodes have not been set");
-    #   if (!_isSetConnectivity)
-    #     errors.Add("Elements have not been set");
+        ## Creating default node id's, if empty
+        if self.NodeIds == None:
+            ## Setting node ids 1,2,3,...
+            self.NodeIds = np.zeros(len(self.X), dtype=int)
+            for i in range(len(self.X)):
+                self.NodeIds[i] = i + 1
+        
+        ## Creating default element id's, if empty
+        if self.ElementIds == None:
+            ## Setting element ids 1,2,3,...
+            self.ElementIds = np.zeros(len(self.__connectivity), dtype=int)
+            for i in range(len(self.__connectivity)):
+                self.ElementIds[i] = i + 1
 
-    #   // Check that all nodenumbers are within the range of
-    #   // number of nodes.
-    #   if (_isSetNodes && _isSetConnectivity)
-    #   {
-    #     bool check = true;
-    #     foreach (int[] elmt in _connectivity)
-    #     {
-    #       foreach (int nodeNumber in elmt)
-    #       {
-    #         if (0 >= nodeNumber || nodeNumber > _x.Length)
-    #         {
-    #           check = false;
-    #           break;
-    #         }
-    #       }
-    #       if (!check)
-    #         break;
-    #     }
-    #     if (!check)
-    #       errors.Add("At least one element has an invalid node number. Node numbers must be within [1,numberOfNodes]");
-    #   }
+        ## Creating additional element information
+        elementType = np.zeros(len(self.__connectivity), dtype=int)
+        nodesPerElmt = np.zeros(len(self.__connectivity), dtype=int)
+        nodeElmtCount = 0  ## total number of nodes listed in the connectivity table
+        for i in range(len(elementType)):
+            elmtTypeNumber = 0
+            elmt = self.__connectivity[i]
+            if len(elmt) == 3:
+                elmtTypeNumber = 21
+            elif len(elmt) == 4:
+                elmtTypeNumber = 25
+            elif len(elmt) == 6:
+                elmtTypeNumber = 32
+            elif len(elmt) == 8:
+                elmtTypeNumber = 33
+            else:
+                raise Exception("Element with invalid number of nodes encountered")
 
-    #   if (dieOnError && errors.Count > 0)
-    #   {
-    #     string msgs = DfsBuilder.ErrorMessage(errors);
-    #     throw new DfsException(msgs);
-    #   }
+            elementType[i] = elmtTypeNumber
+            nodesPerElmt[i] = len(elmt)
+            nodeElmtCount += len(elmt)
 
-    #   return (errors.ToArray());
-    # }
+        connectivityArray = np.zeros(nodeElmtCount, dtype=int)
+        k = 0
+        for i in range(len(elementType)):
+            elmt = self.__connectivity[i]
+            for j in range(len(elmt)):
+                connectivityArray[k] = elmt[j]
+                k += 1
+ 
+        res = MeshFile.Create(self.EumQuantity, 
+                              self.Projection, 
+                              self.NodeIds, 
+                              self.X, 
+                              self.Y,
+                              self.Z, 
+                              self.Code, 
+                              self.ElementIds, 
+                              elementType, 
+                              self.__connectivity)
 
-    # ## <summary>
-    # ## Create and return a new <see cref="MeshFile"/> object
-    # ## </summary>
-    # public MeshFile CreateMesh()
-    # {
-
-    #   Validate(true);
-
-    #   // Creating default eumQuantity in meters
-    #   if (_eumQuantity == null)
-    #     _eumQuantity = new eumQuantity(eumItem.eumIBathymetry, eumUnit.eumUmeter);
-
-    #   // Creating default node id's, if empty
-    #   if (_nodeIds == null)
-    #   {
-    #     // Setting node ids 1,2,3,...
-    #     _nodeIds = new int[_x.Length];
-    #     for (int i = 0; i < _x.Length; i++)
-    #     {
-    #       _nodeIds[i] = i + 1;
-    #     }
-    #   }
-    #   // Creating default element id's, if empty
-    #   if (_elementIds == null)
-    #   {
-    #     // Setting element ids 1,2,3,...
-    #     _elementIds = new int[_connectivity.Length];
-    #     for (int i = 0; i < _connectivity.Length; i++)
-    #     {
-    #       _elementIds[i] = i + 1;
-    #     }
-    #   }
-
-    #   // Creating additional element information
-    #   int[] elementType = new int[_connectivity.Length];
-    #   int[] nodesPerElmt = new int[_connectivity.Length];
-    #   int nodeElmtCount = 0;  // total number of nodes listed in the connectivity table
-    #   for (int i = 0; i < elementType.Length; i++)
-    #   {
-    #     int elmtTypeNumber;
-    #     int[] elmt = _connectivity[i];
-    #     switch (elmt.Length)
-    #     {
-    #       case 3:
-    #         elmtTypeNumber = 21;
-    #         break;
-    #       case 4:
-    #         elmtTypeNumber = 25;
-    #         break;
-    #       case 6:
-    #         elmtTypeNumber = 32;
-    #         break;
-    #       case 8:
-    #         elmtTypeNumber = 33;
-    #         break;
-    #       default:
-    #         // this should have been caught in the validate phase, but just in case:
-    #         throw new DfsException("Element with invalid number of nodes encountered");
-    #     }
-    #     elementType[i] = elmtTypeNumber;
-    #     nodesPerElmt[i] = elmt.Length;
-    #     nodeElmtCount += elmt.Length;
-    #   }
-
-    #   int[] connectivityArray = new int[nodeElmtCount];
-    #   int k = 0;
-    #   for (int i = 0; i < elementType.Length; i++)
-    #   {
-    #     int[] elmt = _connectivity[i];
-    #     for (int j = 0; j < elmt.Length; j++)
-    #     {
-    #       connectivityArray[k++] = elmt[j];
-    #     }
-    #   }
-
-    #   MeshFile res = MeshFile.Create(_eumQuantity, _projection, _nodeIds, _x, _y, _z, _code, _elementIds, elementType, _connectivity);
-
-    #   return (res);
-    # }
+        return res
 
 
     # TODO: do we need this? 
