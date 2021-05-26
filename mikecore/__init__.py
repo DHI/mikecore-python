88bb13500f00b8413c5638f9c27e7a9a3cf3e8e3
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-05-26 11:32:00 +0200

    remove unused

--- mikecore/__init__.py
+++ mikecore/__init__.py
@@ -8,29 +8,19 @@ p = platform.architecture()
 if not "64" in p[0]:
     raise Exception("This library is 64 bit only!!!! Please use 64 bit Python")
 
-# package_directory = os.path.abspath(os.path.dirname(__file__))
-# print(package_directory)
-
-# print(Path(__file__))
-# print(Path(__file__).parent)
-# print(Path(__file__).parent.parent)
-
 if platform.system() == "Windows":
     mikebin = str(Path(__file__).parent / "bin/windows")
     os.environ["PATH"] = mikebin + ";" + os.environ["PATH"]
 elif platform.system() == "Linux":
-    # mikebin = os.path.join(package_directory, "../bin/linux")
     mikebin = str(Path(__file__).parent / "bin/linux")
 else:
     raise Exception("Unsupported platform: " + platform.system())
 
-#os.environ["PATH"] = mikebin + ";" + os.environ["PATH"]
 os.environ["LD_LIBRARY_PATH"] = mikebin
 
 from mikecore.DfsDLL import DfsDLL
 from mikecore.eum import eumDLL
 from mikecore.Projections import MzCartDLL
-
 
 # Path is required for reading EUM.xml
 DfsDLL.libfilepath = mikebin
