350a79478d6ea7c05477963267bbb1f5734e6cf9
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-05-26 11:29:34 +0200

    prepare first release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mikecore",
-    version="0.0.1",
+    version="0.1.0",
     install_requires=["numpy"],
     author="DHI",
     author_email="mike@dhigroup.com",
